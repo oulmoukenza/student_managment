0cbeb7a249178716d049bb4f66cbbe49bdfa868f
Mohamed Yassine Essamadi <78117993+stdynv@users.noreply.github.com>
2022-12-02 13:00:07 +0100

    update student java

--- Student_Mgn_id/src/main/java/Student_Mgn_app/Student_Mgn_id/Student.java
+++ Student_Mgn_id/src/main/java/Student_Mgn_app/Student_Mgn_id/Student.java
@@ -2,4 +2,43 @@ package Student_Mgn_app.Student_Mgn_id;
 
 public class Student {
 
+	private int id;
+	private String nom;
+	private String prenom;
+	private String sexe;
+
+	public Student() {}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getSexe() {
+		return sexe;
+	}
+
+	public void setSexe(String sexe) {
+		this.sexe = sexe;
+	}
+
 }
