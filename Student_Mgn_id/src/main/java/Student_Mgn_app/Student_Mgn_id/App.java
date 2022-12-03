1208e925d23aa1d87fe6ea8c77b9c1983b2c3256
Mohamed Yassine Essamadi <78117993+stdynv@users.noreply.github.com>
2022-12-03 18:20:29 +0100

    choice menu

--- Student_Mgn_id/src/main/java/Student_Mgn_app/Student_Mgn_id/App.java
+++ Student_Mgn_id/src/main/java/Student_Mgn_app/Student_Mgn_id/App.java
@@ -28,9 +28,11 @@ public class App {
 			switch(choice) {
 				case 1 : 
 					System.out.println("Student");
+					MenuEtudiant();
 					break;
 				case 2 : 
 					System.out.println("Admin");
+					MenuAdmin();
 					break;
 				case 3 : 
 					System.out.println("Bye !");
@@ -40,22 +42,68 @@ public class App {
 
 		}
 		
-		// case 1 Student
-		/**
-		 * 1- display information
-		 
-		 * 2 - Exit
-		 * **/
 		
-		//case 2 Admin 
-		/**
-		 * 1- Add Record
-		 * 2 - Update Record
-		 * 3- display all Student / {id}
-		 *  4- Delete Record {id}
-		 *  5 - Logout
-		 * **/
 
+		
+	}
+
+	private static void MenuAdmin() {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome To Admin Dashboard");
+		Scanner scan_2 = new Scanner(System.in);
+		System.out.println("1- List All students : ");
+		System.out.println("2- Add New student : ");
+		System.out.println("3- Update student informations: ");
+		System.out.println("4- Delete students : ");
+		System.out.println("5 - Menu Principal ");
+		int ChoixMenuAdmin = scan_2.nextInt();
+		switch(ChoixMenuAdmin) {
+		case 1 : 
+			System.out.println("All Students Informations");
+			MenuAdmin();
+			break;
+		case 2 :
+			System.out.println("Add New Student");
+			MenuAdmin();
+			break;
+		case 3 : 
+			System.out.println("Update Student Information");
+			MenuAdmin();
+			break;
+		case 4 : 
+			System.out.println("Delete Student");
+			MenuAdmin();
+			break;
+		case 5 : 
+			System.out.println("Delete Student");
+			break;
+		default:
+	        System.out.println("Choix incorrect");
+	        MenuAdmin();
+	        break;
+		}
+	}
+	private static void MenuEtudiant() {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome To Student Dashboard");
+		Scanner scan_1 = new Scanner(System.in);
+		System.out.println("1- Display Information : ");
+		System.out.println("2- Exit : ");
+		int ChoixMenuEtudiant = scan_1.nextInt();
+		switch(ChoixMenuEtudiant) {
+		case 1 : 
+			System.out.println("Informations");
+			MenuEtudiant();
+			break;
+		case 2 :
+			System.out.println("Redirect Menu Principal ");
+			break;
+		default:
+	        System.out.println("Choix incorrect");
+	        MenuEtudiant();
+	        break;
+		}
+		
 		
 	}
 
